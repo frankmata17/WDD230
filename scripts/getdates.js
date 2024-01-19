4e9858e6355ad57f5253aeda6d5019a8da5b6313
frankmata17 <frank.mata17@gmail.com>
2024-01-18 21:20:09 -0700

    updated home page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,8 +6,7 @@ const currentYear = new Date().getFullYear();
 // Set the copyright year in the footer's first paragraph
 document.querySelector('footer p:first-child').innerHTML = `&copy; Francisco Mata ${currentYear} Utah`;
 
-// Get the last modified date
+
 const lastModifiedDate = document.lastModified;
 
-// Set the last modified date in the second paragraph with id "lastModified"
 document.getElementById('lastModified').innerHTML = `Last Modified: ${lastModifiedDate}`;
