e8d1086f76c3bab9f506075d260d1c03987b9906
frankmata17 <frank.mata17@gmail.com>
2024-02-06 22:16:35 -0700

    w05 updates

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuButton = document.getElementById('menu');
     const navigation = document.querySelector('.navigation');
+    const darkModeToggle = document.querySelector('.switch input');
+    const navigationMenu = document.querySelector('.navigation');
+
+    darkModeToggle.addEventListener('change', function () {
+        const isDarkMode = darkModeToggle.checked;
+        document.body.classList.toggle('dark-mode', isDarkMode);
+        document.querySelector('header').classList.toggle('dark-mode', isDarkMode);
+        document.querySelector('footer').classList.toggle('dark-mode', isDarkMode);
+        navigationMenu.classList.toggle('dark-mode', isDarkMode);
+    });
+
 
     menuButton.addEventListener('click', function () {
         navigation.classList.toggle('show-menu');
